43535779fa176a63b669e3ac76dfaec68d1b1532
kikiherliawan <kikiherliawann@gmail.com>
2022-10-19 16:48:02 +0700

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div className="h-6 bg-black">Navbar</div>;
+  return (
+    <div className="h-20 bg-black">
+      <p className=" text-green-600 pt-6 pl-10 font-extrabold text-lg">
+        Doomer
+      </p>
+    </div>
+  );
 };
 
 export default Navbar;
