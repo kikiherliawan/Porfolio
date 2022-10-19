63fcd353b602b9b9705ec30ae09905230de22ef0
kikiherliawan <kikiherliawann@gmail.com>
2022-10-19 16:06:13 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
-    <div className="text-6xl">
-      <h1>Najma Murdhia Halaby</h1>
+    <div>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div className="h-6 bg-black">Navbar</div>;
+};
+
+export default Navbar;
